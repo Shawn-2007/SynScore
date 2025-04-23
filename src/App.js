67fc20f50378085973299f2457ef3a9c18ccd103
Wang <pipaeppe@hotmail.com>
2025-04-24 01:44:02 +0800

    1.1.5

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './Home';
 import Scoreboard from './Scoreboard';
 
 function App() {
   return (
     <Router basename="/SynScore">
+      {/* <Router> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/scoreboard" element={<Scoreboard />} />
+        {/* 處理所有未匹配的路由，重定向到主頁 */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
 
